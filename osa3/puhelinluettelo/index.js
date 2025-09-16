63b3925f6298cb427c59b6d6c7f85d3a7e79f174
Kalle Kauppila <kallekauppila@kamk.fi>
2025-09-16 22:22:22 +0300

    fixed port

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -88,8 +88,7 @@ app.post("/api/persons", (request, response) => {
     response.json(puhelinnumero)
 })
 
-const PORT = 3001
-
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
