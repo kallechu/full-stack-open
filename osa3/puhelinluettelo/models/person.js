03ce0e06474685f6644913cee62af013ac30e714
Kalle Kauppila <kallekauppila@kamk.fi>
2026-01-21 11:37:52 +0200

    3.21

--- osa3/puhelinluettelo/models/person.js
+++ osa3/puhelinluettelo/models/person.js
@@ -19,7 +19,10 @@ const personSchema = new mongoose.Schema({
       minlength: 3,
       required: true
     },
-    number: String,
+    number: {
+      type: String,
+      minlength: 8,
+    }
 })
 
 personSchema.set('toJSON', {
