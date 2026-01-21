510a2364f11c813e23c09071a30ae8aca9c58c73
Kalle Kauppila <kallekauppila@kamk.fi>
2026-01-21 13:47:23 +0200

    bug fix

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require("./controllers/blogs")
+const config = require('./utils/config')
 
 const app = express()
 
-mongoose.connect(mongoUrl, { family: 4 })
+mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 app.use(express.json())
 
